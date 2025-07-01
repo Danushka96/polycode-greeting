33b7a49dbcc87af4f20df2c6b597e8024af05352
danushka96 <danushkaherath96@gmail.com>
2025-07-01 16:02:19 +0530

    fix: added description field

--- services/greeting-service/Greeting.go
+++ services/greeting-service/Greeting.go
@@ -5,6 +5,7 @@ import (
 	"portal/register/model"
 )
 
+// @description return a greeting message for given name
 func Greeting(ctx polycode.ServiceContext, input model.HelloRequest) (model.HelloResponse, error) {
 	return model.HelloResponse{
 		Message: "Hello" + input.Name,
